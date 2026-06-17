9ab1da4e6fde99924f2a15cfd0fe8fbdd0975d98
bashir6969 <skibidivandijk4@gmail.com>
2026-06-17 14:51:37 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDo9bvP0vFfhf3AW3VUs6O0QsAshGz1Eo",
+  authDomain: "d-originals-dcea4.firebaseapp.com",
+  projectId: "d-originals-dcea4",
+  storageBucket: "d-originals-dcea4.firebasestorage.app",
+  messagingSenderId: "648644075897",
+  appId: "1:648644075897:web:65fd59fbe65b25c759cfa8"
+};
