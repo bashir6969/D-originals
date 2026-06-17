5406a5b1226c69d2fc6cba36d886323b69547615
bashir6969 <skibidivandijk4@gmail.com>
2026-06-17 20:19:16 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDo9bvP0vFfhf3AW3VUs6O0QsAshGz1Eo",
@@ -8,3 +9,7 @@ export const firebaseConfig = {
   messagingSenderId: "648644075897",
   appId: "1:648644075897:web:65fd59fbe65b25c759cfa8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
